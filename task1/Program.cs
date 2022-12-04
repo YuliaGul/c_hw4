35867ef1d05acd4cee67976fede8f739025d24a5
yu_gulyaeva <mangostinshop@mail.ru>
2022-12-04 22:19:24 +0300

    Задача 1: возведение в степень

--- task1/Program.cs
+++ task1/Program.cs
@@ -2,4 +2,20 @@
 //Math.Pow использовать нельзя
 //3, 5 -> 243 (3⁵)
 //2, 4 -> 16
-
+int DegreeFunction (int number, int degree)
+{
+    int i = 0;
+    int result = 1; 
+    while (i < degree)
+    {
+        result *= number;
+        i++;
+    }
+    return result;
+}
+Console.Write("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int res = DegreeFunction (number1, number2);
+Console.WriteLine (res);
