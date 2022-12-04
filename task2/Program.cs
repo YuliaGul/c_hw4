87861d72a4f520329547fae4a7680cc1e3d4f87a
yu_gulyaeva <mangostinshop@mail.ru>
2022-12-04 22:32:51 +0300

    Задача 2: сумма цифр

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,3 +2,34 @@
 //452 -> 11
 //82 -> 10
 //9012 -> 12
+int LenNumber (int number)
+{
+    int length = 0;
+    while(number>0)
+    {
+        number= number/10;
+        length ++;
+    }
+    return length;
+}
+
+int SumNumber(int number, int length)
+{
+    int result = 0;
+    int i = 0;
+    while(i<length)
+    {
+        result+=number%10;
+        number=number/10;
+        i++;
+    }
+    return result;
+
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int len = LenNumber (number);
+Console.WriteLine (len);
+int res = SumNumber(number, len);
+Console.WriteLine (res);
+
